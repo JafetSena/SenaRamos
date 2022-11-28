bb84903eca4624b7ec4b522ab205136bcbc65f48
JafetSena <jasena2002@hotmail.com>
2022-11-28 16:48:32 -0600

    Llenar grid nuevo push

--- Parcial 3/Grid/GridJs.js
+++ Parcial 3/Grid/GridJs.js
@@ -26,26 +26,27 @@ $(document).ready(function(){
     $.ajax(settings).done(function(response){
         console.log(response);
         var tabla = JSON.parse(response);
-    });
-    $("#jsGrid").jsGrid({
-        width: "100%",
-        height: "400px",
-     
-        inserting: true,
-        editing: true,
-        sorting: true,
-        paging: true,
-     
-        data: tabla,
-     
-        fields: [
-            { name: "IdEmpleado", type: "text", width: 150, validate: "required" },
-            { name: "nombre", type: "number", width: 50 },
-            { name: "apPaterno", type: "text", width: 200 }
-            // { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-            // { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-            // { type: "control" }
-        ]
+    
+        $("#jsGrid").jsGrid({
+            width: "100%",
+            height: "400px",
+         
+            inserting: true,
+            editing: true,
+            sorting: true,
+            paging: true,
+         
+            data: tabla,
+         
+            fields: [
+                { name: "IdEmpleado", type: "text", width: 150, validate: "required" },
+                { name: "nombre", type: "number", width: 50 },
+                { name: "apPaterno", type: "text", width: 200 }
+                // { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
+                // { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
+                // { type: "control" }
+            ]
+        });
     });
     
 });
