94264b2ea81e7946bc56828130cba1c4ccb93e00
JafetSena <jasena2002@hotmail.com>
2022-11-30 07:55:06 -0600

    Ejercicio en clases Cookies

---
+++ Parcial 3/Cookies/Cookies.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cookies</title>
+</head>
+<body>
+<?php
+    if(isset($_COOKIE['ultimaVisita']))
+    {
+        printf("Su ultima vista fue : %s",$_COOKIE['ultimaVisita']);
+
+        $fechaHoy = date('Y/m/d H:i:s');
+        setcookie("ultimaVisita",$fechaHoy, time() + (7 * 24 * 60 * 60));
+    }
+    else {
+        printf("No se encontró cookie de útlima visita<br>");
+
+        $fechaHoy = date('Y/m/d H:i:s');
+        $siguienteSemana = time() + (7 * 24 * 60 * 60);
+        $fechaExpira = date('Y/m/d H:i:s', $siguienteSemana).'<br>';
+
+        echo '<b>Fecha de Creación de Cookie: </b>'.$fechaHoy.'<br>';
+        echo '<b>Fecha de Vencimiento de Cookie: </b>'.$fechaHoy.'<br>';
+
+        setcookie("ultimaVisita",$fechaHoy, time() + (7 * 24 * 60 * 60));
+    }
+
+    //setcookie("últimaVisita: " time(), -3600)
+?>
+</body>
+
+</html>
